3df9861c57cdc454b446d1e3a651b58402946c48
Ioannis Fanourakis <102972970+GiannisFanourakis@users.noreply.github.com>
2026-09-15 10:55:35 +0300

    Format regional Google imagery allowlist

--- src/config/googlePlacesAllowlist.ts
+++ src/config/googlePlacesAllowlist.ts
@@ -9,10 +9,7 @@ export interface GooglePlacesAllowlistEntry {
   coordinates: [number, number];
 }
 
-const AUDITED_GOOGLE_PLACE_PRODUCERS = [
-  ...CRETAN_PRODUCERS,
-  ...SANTORINI_PRODUCERS,
-];
+const AUDITED_GOOGLE_PLACE_PRODUCERS = [...CRETAN_PRODUCERS, ...SANTORINI_PRODUCERS];
 
 /**
  * Eligibility comes only from manually audited regional catalogues.
