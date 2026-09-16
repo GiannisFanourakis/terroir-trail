186010170840c00e3bf486a22e79080f0714f9de
Ioannis Fanourakis <102972970+GiannisFanourakis@users.noreply.github.com>
2026-09-16 09:08:01 +0300

    Install account cache privacy guard

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { AuthEmailDeliveryNotice } from './components/Auth/AuthEmailDeliveryNotice';
 import { AppErrorBoundary } from './components/System/AppErrorBoundary';
+import { installAccountCachePrivacyGuard } from './services/accountCachePrivacy';
 import { initGlobalErrorHandlers } from './services/globalErrorHandlers';
 import './index.css';
 
 initGlobalErrorHandlers();
+installAccountCachePrivacyGuard();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
